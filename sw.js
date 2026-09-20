fe35246c4781e86a962c70efcbb01304c31cd494
store <sahbanmajed@gmail.com>
2026-09-20 21:53:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('fetch', function (event) {
-  // يقوم بتمرير الطلبات بشكل طبيعي دون تعقيد
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
